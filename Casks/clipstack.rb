97e4a7175884dfbaf3d37366d81e8d8d487152ef
Vladimir Weinstein <github@weivsara.com>
2026-02-17 18:04:11 -0800

    Update ClipStack to v0.1.2 with multi-type clipboard support

--- Casks/clipstack.rb
+++ Casks/clipstack.rb
@@ -1,6 +1,6 @@
 cask "clipstack" do
-  version "0.1.1"
-  sha256 "eeafad659a0f332e82e77ed845246382bfdf7de7e7ec967970db91cd9e2efccb"
+  version "0.1.2"
+  sha256 "275ed0e427549de6fa6e51eddd048aaf6fdea3a1d14ee982a96b881c7472be0f"
 
   url "https://github.com/weiv/clipstack/releases/download/v#{version}/ClipStack.dmg"
   name "ClipStack"
